1350b8877e33c60bedf54497c2fd9a267a906da4
yaowen <Wen.Yao2@geely.com>
2020-02-09 23:19:39 +0800

    flutter demo更新

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Welcome to Flutter',
+      theme: new ThemeData(
+          primaryColor: Colors.yellow
+      ),
       home: new RandomWords(),
     );
   }
@@ -23,6 +26,7 @@ class RandomWords extends StatefulWidget {
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
+  final _saved = new Set<WordPair>();
 
   @override
   Widget build(BuildContext context) {
@@ -32,6 +36,9 @@ class RandomWordsState extends State<RandomWords> {
       return new Scaffold(
         appBar: new AppBar(
           title: new Text("名字生成器"),
+          actions: <Widget>[
+            new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved)
+          ],
         ),
         body: _buildSuggestions(),
       );
@@ -53,10 +60,59 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair){
+    final alreadySaved = _saved.contains(pair);
+
     return new ListTile(
       title: new Text(
         pair.asPascalCase,
         style: _biggerFont,
+      ),
+
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: (){
+        setState(() {
+          if (alreadySaved){
+            _saved.remove(pair);
+          }else{
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+
+
+  void _pushSaved(){
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+          builder: (context){
+            final tiles = _saved.map(
+                (pair){
+                  return new ListTile(
+                    title: new Text(pair.asPascalCase,
+                    style: _biggerFont,
+                    ),
+                  );
+                }
+            );
+
+            final divided = ListTile
+                .divideTiles(
+                context: context,
+                tiles: tiles,
+            )
+            .toList();
+
+            return new Scaffold(
+              appBar: new AppBar(
+                title: new Text('saved suggesions'),
+              ),
+              body: new ListView(children: divided),
+            );
+          }
       ),
     );
   }
